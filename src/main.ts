2cdde8f1b1d90cf1c977ab9bf1d62efc94adc145
biliqis <biliqistemmy@gmail.com>
2022-04-27 06:52:07 +0100

    edited the port

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envConfiguration } from './utils/config/envConfiguration';
 import 'dotenv/config';
+import { ConfigService } from '@nestjs/config';
+const configService = new ConfigService()
+const PORT = configService.get<'string'>(envConfiguration.PORT)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
-  await app.listen(envConfiguration.PORT);
+  await app.listen(PORT);
 
-  console.log('app is running on port 3000');
+  console.log(`app is running on port ${PORT}`);
 }
 bootstrap();
